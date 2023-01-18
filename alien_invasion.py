eed483ae3e51f37d64b4bb52b78a6e1617181e55
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-01-18 06:03:45 +0530

    Full screen mode. q or esc to quit.

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,9 +15,16 @@ class AlienInvation:
 
         self.settings = Settings()
 
-        # Set the game screen window and title.
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        # Set the screen: windowed mode
+        # self.screen = pygame.display.set_mode(
+        #     (self.settings.screen_width, self.settings.screen_height))
+
+        # Set the screen: fullscreen mode
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        
+        # Set the game window caption
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -46,7 +53,7 @@ class AlienInvation:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
-        elif event.key == pygame.K_q:
+        elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
             sys.exit()
 
     def _check_keyup_events(self, event):
