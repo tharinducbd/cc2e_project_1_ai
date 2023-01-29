842aaf7338708cbf6e7852f8ba2c38772c6fbdb0
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-01-29 17:41:48 +0530

    Modified main game loop.

--- alien_invasion.py
+++ alien_invasion.py
@@ -42,9 +42,12 @@ class AlienInvation:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
 
     def _check_events(self):
