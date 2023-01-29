806531ec728da10c02bb08f4bd563cd865c3fece
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-01-29 07:47:12 +0530

    ai update: ship hit basics

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import sys
+from time import sleep
 
 import pygame
 
@@ -6,6 +7,7 @@ from settings import Settings
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
+from game_stats import GameStats
 
 
 class AlienInvation:
@@ -25,6 +27,9 @@ class AlienInvation:
 
         # Set the game window caption
         pygame.display.set_caption("Alien Invasion")
+
+        # Create an instance to store game statistics.
+        self.stats = GameStats(self)
 
         # Game object instances
         self.ship = Ship(self)
@@ -110,7 +115,7 @@ class AlienInvation:
 
         # Look for alien-ship collisions.
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
-            print("Ship hit!!!")
+            self._ship_hit()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
@@ -155,6 +160,23 @@ class AlienInvation:
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1
+
+    def _ship_hit(self):
+        """Respond to the ship being hit by an alien."""
+
+        # Decrement ships_left.
+        self.stats.ships_left -= 1
+
+        # Get rid of any remaining aliens and bullets.
+        self.aliens.empty()
+        self.bullets.empty()
+
+        # Create a new fleet and center the ship.
+        self._create_fleet()
+        self.ship.center_ship()
+
+        # Pause.
+        sleep(0.5)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
