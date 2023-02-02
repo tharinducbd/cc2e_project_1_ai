4db6abc5eeccdcb019370e40f8d5b9e81b0adfbe
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-02 06:21:23 +0530

    Button class: initialize, dimensions.

--- button.py
+++ button.py
@@ -1,1 +1,19 @@
 import pygame.font
+
+
+class Button:
+    """A class to create a Button."""
+
+    def __init__(self, ai_game, msg) -> None:
+        """Initialize button attributes."""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        # Set the dimensions and properties of the button.
+        self.width, self.height = (200, 50)
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Build the button object and center it.
+        
