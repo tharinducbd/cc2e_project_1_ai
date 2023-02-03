ff0b8afd299c2e515a238473b8262b5d051bad30
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-03 21:02:03 +0530

    minor

--- scoreboard.py
+++ scoreboard.py
@@ -53,7 +53,7 @@ class Scoreboard:
             self.prep_high_score()
 
     def show_score(self):
-        """Draw score to the screen."""
+        """Draw scores and levels to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
