289f51ef4b583a7431af5c725c99eec0dc0870ac
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-03 06:45:10 +0530

    Button attempt 1

--- alien_invasion.py
+++ alien_invasion.py
@@ -215,6 +215,10 @@ class AlienInvation:
         # Draw aliens
         self.aliens.draw(self.screen)
 
+        # Draw the play button if the game is inactive.
+        if not self.stats.game_active:
+            self.play_button.draw_button()
+
         # Update the full display Surface to the screen.
         pygame.display.flip()
 
