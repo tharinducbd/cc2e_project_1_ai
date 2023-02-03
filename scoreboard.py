0eb80a122dc9be672829c2158b7c57046a01ea2c
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-03 20:43:17 +0530

    high score prep and check

--- scoreboard.py
+++ scoreboard.py
@@ -6,7 +6,6 @@ class Scoreboard:
 
     def __init__(self, ai_game) -> None:
         """Initialize scorekeeping attributes."""
-
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
@@ -16,12 +15,12 @@ class Scoreboard:
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
 
-        # Prepare the initial score image.
+        # Prepare the initial score images.
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
         """Turn the score into a rendered image."""
-
         rounded_score = round(self.stats.score, -1)
         score_str = "{:,}".format(rounded_score)
 
@@ -33,6 +32,26 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """Turn the high score into a rendered image."""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+
+        self.high_score_image = self.font.render(
+            high_score_str, True, self.text_color, self.settings.bg_color)
+        
+        # Center the high score at the top of the screen.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+    def check_high_score(self):
+        """Check to see if there's a new high score."""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
+
     def show_score(self):
         """Draw score to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
