cd7341158fdacee66832c8cd4d1f6a824e51f946
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-03 20:58:12 +0530

    level blit

--- scoreboard.py
+++ scoreboard.py
@@ -56,6 +56,7 @@ class Scoreboard:
         """Draw score to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
 
     def prep_level(self):
         """Turn the level into a rendered image."""
