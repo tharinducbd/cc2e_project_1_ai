768d9d665ad208f297523f01d12de95cc491618e
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-02-04 06:37:27 +0530

    Sprite inheritance added

--- ship.py
+++ ship.py
@@ -1,11 +1,13 @@
 import pygame
+from pygame.sprite import Sprite
 
 
-class Ship:
+class Ship(Sprite):
     """A class to manage the ship."""
 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
+        super().__init__()
 
         # Get the game screen and get its rect.
         self.screen = ai_game.screen
